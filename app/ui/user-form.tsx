687fc433769d8d202e6fa878cd3a2704484197b5
luciaduarte <lucia.duarte@atlasteam.de>
2024-11-14 15:55:39 -0300

    -Added form to save instance data

---
+++ app/ui/user-form.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { lusitana } from "@/app/ui/fonts";
+import {
+  AtSymbolIcon,
+  KeyIcon,
+  GlobeAltIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { Button } from "@/app/ui/button";
+import { useActionState } from "react";
+import { saveUserInstanceData, State } from "@/app/lib/actions";
+import { useState } from "react";
+
+export default function UserForm() {
+  const initialState: State = { message: null, errors: {} };
+
+  const [authToken, setAuthToken] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [instance, setInstance] = useState<string>("");
+  const [state, formAction] = useActionState(
+    saveUserInstanceData,
+    initialState
+  );
+  const errorMessage = state.message;
+
+  return (
+    <form action={formAction} className="space-y-3">
+      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+          Configure your Atlassian user.
+        </h1>
+        <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="authToken"
+            >
+              Auth Token
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="authToken"
+                type="text"
+                name="authToken"
+                placeholder="Auth Token"
+                value={authToken}
+                onChange={(e) => setAuthToken(e.target.value)}
+                required
+              />
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="email"
+            >
+              User E-mail
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="instance"
+            >
+              Instance url
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="instance"
+                type="text"
+                name="instance"
+                placeholder="Instance Url"
+                value={instance}
+                onChange={(e) => setInstance(e.target.value)}
+                required
+              />
+              <GlobeAltIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+          </div>
+        </div>
+        <Button className="mt-4 w-full" aria-disabled={true}>
+          Save <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+        </Button>
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
+        </div>
+      </div>
+    </form>
+  );
+}
