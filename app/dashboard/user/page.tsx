6b8eb6fc6955a57deacac9a559d9cb181f4b6b84
luciaduarte <lucia.duarte@atlasteam.de>
2024-11-14 15:58:40 -0300

    -User tab showing form & instance data available

--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <p>Users Page</p>;
+import UserForm from "@/app/ui/user-form";
+import { fetchInstanceData } from "@/app/lib/data";
+import { Card } from "@/app/ui/dashboard/cards";
+export default async function Page() {
+  const instanceData = await fetchInstanceData();
+  return (
+    <>
+      <UserForm />
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        {instanceData.map((instance) => (
+          <Card
+            key={instance.authToken}
+            title={`Instance: ${instance.instance}`}
+            value={instance.email}
+            type="customers"
+          />
+        ))}
+      </div>
+    </>
+  );
 }
