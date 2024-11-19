494188776ee0aa31891243f5518d08d6dce37805
luciaduarte <lucia.duarte@atlasteam.de>
2024-11-18 21:35:55 -0300

    - Deleted cards from dashboard main page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,12 +7,7 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={"4"} type="collected" />
-        <Card title="Pending" value={"3"} type="pending" />
-        <Card title="Total Invoices" value="1" type="invoices" />
-        <Card title="Total Customers" value={111111} type="customers" />
-      </div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"></div>
     </main>
   );
