6cbfc7abdb685689728945b6e6dbb210055442d1
luciaduarte <lucia.duarte@atlasteam.de>
2024-11-21 17:53:13 -0300

    - Fix: change input type for authPass to password

--- app/ui/user/createForm.tsx
+++ app/ui/user/createForm.tsx
@@ -44,7 +44,7 @@ export default function CreateForm() {
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="authToken"
-                type="text"
+                type="password"
                 name="authToken"
                 placeholder="Auth Token"
                 value={authToken}
